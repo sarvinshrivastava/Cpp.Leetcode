62e94d517bd50156e346936fa7719a388854fa72
Sarvin Shrivastava <sarvin5124@gmail.com>
2024-12-30 21:14:21 +0530

    Solved problem no 2466!

---
+++ P-2466.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp(high + 1);
+        dp[0] = 1;
+        int mod = 1000000007;
+
+        for (int end = 1; end <= high; ++end) {
+            if (end >= zero) {
+                dp[end] += dp[end - zero];
+            }
+            if (end >= one) {
+                dp[end] += dp[end - one];
+            }
+            dp[end] %= mod;
+        }
+
+        int answer = 0;
+        for (int i = low; i <= high; ++i) {
+            answer += dp[i];
+            answer %= mod;
+        }
+        return answer;
+    }
+};
