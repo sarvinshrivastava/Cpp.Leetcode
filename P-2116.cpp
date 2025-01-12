59f35df3024cdb52697ff89504f0b2a5d5ef9865
Sarvin Shrivastava <sarvin5124@gmail.com>
2025-01-12 20:24:51 +0530

    Solved problem no 2116!

---
+++ P-2116.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int len = s.size();
+        if (length % 2 == 1)
+            return false;
+
+        stack<int> opnb, unl;
+
+        for (int i = 0; i < length; i++) {
+            if (locked[i] == '0') {
+                unl.push(i);
+            } else if (s[i] == '(') {
+                opnb.push(i);
+            } else if (s[i] == ')') {
+                if (!opnb.empty()) {
+                    opnb.pop();
+                } else if (!unl.empty()) {
+                    unl.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!opnb.empty() && !unl.empty() &&
+               opnb.top() < unl.top()) {
+            opnb.pop();
+            unl.pop();
+        }
+
+        if (!opnb.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+};
