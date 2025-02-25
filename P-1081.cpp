816c80aa954179a98498f5c33f2218a63541e729
Sarvin Shrivastava <sarvin5124@gmail.com>
2025-02-25 20:08:25 +0530

    Solved problem no 1081!

---
+++ P-1081.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        unordered_map<char, int> freq;
+
+    for (auto ch : s)
+        freq[ch]++;
+
+    vector<bool> isPresent(26, false);
+    string result;
+    stack<char> mono;
+
+    for (auto ch : s)
+    {
+        freq[ch]--;
+        if (isPresent[ch - 'a'])
+            continue;
+
+        while (!mono.empty() && mono.top() > ch && freq[mono.top()] > 0)
+        {
+            isPresent[mono.top() - 'a'] = false;
+            mono.pop();
+        }
+
+        mono.push(ch);
+        isPresent[ch - 'a'] = true;
+    }
+    while (!mono.empty())
+    {
+        result = mono.top() + result;
+        mono.pop();
+    }
+
+    return result;
+    }
+};
